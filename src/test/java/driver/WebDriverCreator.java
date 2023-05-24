7d175c1432bc3c8bb021ba32a673380d37b501c0
Aydinv13 <a.baydarov@adguard.com>
2023-05-24 14:29:17 +0300

    Added driver package

---
+++ src/test/java/driver/WebDriverCreator.java
@@ -0,0 +1,37 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverCreator {
+    private static final String YANDEX_DRIVER_PATH = String.format("%s,%s", System.getenv("BROWSER_DRIVERS"), "chromedriver-yandex");
+    private static final String YANDEX_BROWSER_PATH = "/Applications/Yandex.app/Contents/MacOS/Yandex";
+
+    public static WebDriver createWebDriver() {
+        String browser = System.getProperty("browser");
+        if(browser == null) {
+            return createChromeDriver();
+        }
+        switch (browser) {
+            case "yandex":
+                return createYandexDriver();
+            case "chrome":
+            default:
+                return createChromeDriver();
+        }
+    }
+    private static WebDriver createChromeDriver() {
+        System.setProperty("webDriver.chrome.driver", YANDEX_DRIVER_PATH);
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBinary(YANDEX_BROWSER_PATH);
+        return new ChromeDriver(chromeOptions);
+    }
+    private static WebDriver createYandexDriver() {
+        System.setProperty("webDriver.chrome.driver", YANDEX_DRIVER_PATH);
+        ChromeOptions yandexOptions = new ChromeOptions();
+        yandexOptions.setBinary(YANDEX_BROWSER_PATH);
+        return new ChromeDriver(yandexOptions);
+    }
+}
+
